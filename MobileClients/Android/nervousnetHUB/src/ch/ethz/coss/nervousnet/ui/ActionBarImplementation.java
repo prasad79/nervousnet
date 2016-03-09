97ab0706b5aa04674ad2dcc94c423af90486df0b
Prasad <prasad7779@gmail.com>
2016-03-09 12:51:15 +0100

    Added ActionBarImplementation to be used for implementing ActionBar throughout Activities

---
+++ MobileClients/Android/nervousnetHUB/src/ch/ethz/coss/nervousnet/ui/ActionBarImplementation.java
@@ -0,0 +1,10 @@
+package ch.ethz.coss.nervousnet.ui;
+
+import android.app.ActionBar;
+import android.widget.Switch;
+
+public interface ActionBarImplementation {
+	
+	
+	public void updateActionBar();
+}
