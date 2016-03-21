4dae8bbb264da812b0d7bdcee0cfdd8a3bde402c
Prasad <prasad7779@gmail.com>
2016-03-21 12:57:51 +0100

    Added Utils.java

---
+++ MobileClients/Android/nervousnetLIB/src/ch/ethz/coss/nervousnet/lib/Utils.java
@@ -0,0 +1,30 @@
+package ch.ethz.coss.nervousnet.lib;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
+public class Utils {
+
+	public static void displayAlert(Context context, String title, String message, String posButtonTitle,
+			OnClickListener posOnClickListener, String negButtonTitle, OnClickListener negOnClickListener) {
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+		// set title
+		alertDialogBuilder.setTitle(title);
+
+		// set dialog message
+		alertDialogBuilder.setMessage(message).setCancelable(false).setPositiveButton(posButtonTitle,
+				posOnClickListener);
+		if (negButtonTitle != null) {
+			alertDialogBuilder.setNegativeButton(negButtonTitle, negOnClickListener);
+		}
+
+		// create alert dialog
+		AlertDialog alertDialog = alertDialogBuilder.create();
+
+		// show it
+		alertDialog.show();
+	}
+}
