c7249edfa7cb4605551884b0c32b379997c15384
Prasad <prasad7779@gmail.com>
2016-04-01 14:26:58 +0200

    Added Authenctication controller

---
+++ MobileClients/Android/nervousnetHUB/src/ch/ethz/coss/nervousnet/authentication/AuthenticationController.java
@@ -0,0 +1,20 @@
+package ch.ethz.coss.nervousnet.authentication;
+
+public class AuthenticationController {
+
+	
+	/*
+	 * Function to check if the Application requesting for data is authorized and has the right auth token.
+	 */
+	public boolean checkAuthToken(long authToken, String appName) {
+		
+		//return true for all applications for now.
+		return true;	
+	}
+	
+	
+	public boolean checkSensorPermission(String appName) {
+		//return true for all sensors for now.
+		return true;
+	}
+}
---
+++ MobileClients/Android/nervousnetHUB/src/ch/ethz/coss/nervousnet/authentication/SensorAuthentication.java
@@ -0,0 +1,48 @@
+package ch.ethz.coss.nervousnet.authentication;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import ch.ethz.coss.nervousnet.lib.LibConstants;
+
+public class SensorAuthentication {
+
+	private Hashtable<Integer, Byte> sensorAuthList = new Hashtable<Integer, Byte>();
+	
+	public void SensorAuthentication() {
+		sensorAuthList.put(LibConstants.SENSOR_ACCELEROMETER, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_BATTERY,(byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_BLEBEACON,(byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_CONNECTIVITY, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_DEVICE, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_GYROSCOPE, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_HUMIDITY, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_LIGHT, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_LOCATION, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_MAGNETIC, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_NOISE, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_PRESSURE, (byte)0);
+		sensorAuthList.put(LibConstants.SENSOR_PROXIMITY, (byte)0);
+	}
+	
+	
+	public void updateSensorAuthentication(int sensorID, byte accessRightsValue) {
+		sensorAuthList.put(sensorID, accessRightsValue);
+	}
+	
+	public String getSensorAuthenticationString(){
+		String buffer = "";
+		for (int key : sensorAuthList.keySet()) {
+			buffer = +key+"|"+sensorAuthList.get(key);
+		}
+		
+		return buffer;
+	}
+	
+	
+	public void parseSensorAuthenticationString(String sensorString) {
+		
+	}
+	
+	
+}
